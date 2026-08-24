51726397e670e0a2fd14d4c48e3568ed56138231
Donugob <donugob1@gmail.com>
2026-08-24 19:42:36 +0100

    chore: add harfbuzzjs to serverExternalPackages to prevent wasm resolve error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ['@resvg/resvg-js', '@react-pdf/renderer'],
+  serverExternalPackages: ['@resvg/resvg-js', '@react-pdf/renderer', 'satori', 'harfbuzzjs'],
 };
 
 export default nextConfig;
