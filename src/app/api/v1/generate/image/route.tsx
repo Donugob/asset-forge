b07eaf8afedb5c4be21eeb98f517173893cb3ba0
Donugob <donugob1@gmail.com>
2026-09-02 10:06:20 +0100

    fix: duplicate import in image route

--- src/app/api/v1/generate/image/route.tsx
+++ src/app/api/v1/generate/image/route.tsx
@@ -1,4 +1,3 @@
-import { NextResponse } from "next/server";
 import { NextResponse, NextRequest } from "next/server";
 import { generatePayloadSchema } from "@/lib/schema";
 import satori from "satori";
